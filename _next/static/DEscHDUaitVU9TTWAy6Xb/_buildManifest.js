6258347a662572c6b67cc3cfbc58068239399b2c
Lumexio <trotroyu@gmail.com>
2025-10-29 04:18:05 -0600

    Updates

--- _next/static/DEscHDUaitVU9TTWAy6Xb/_buildManifest.js
+++ _next/static/DEscHDUaitVU9TTWAy6Xb/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/a-a-battery-route-problem//_next/:path+",
-        "destination": "/a-a-battery-route-problem/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
