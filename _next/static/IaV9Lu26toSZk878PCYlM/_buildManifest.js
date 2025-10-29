0db0c8c7fe7fe124217837737543b060b18716da
Lumexio <trotroyu@gmail.com>
2025-10-29 04:14:00 -0600

    Updates

---
+++ _next/static/IaV9Lu26toSZk878PCYlM/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/a-a-battery-route-problem//_next/:path+",
+        "destination": "/a-a-battery-route-problem/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
