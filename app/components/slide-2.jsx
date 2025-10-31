6c102a40a36c608c1d1d71290dda662d06165f66
lumexio <trotroyu@gmail.com>
2025-10-31 16:36:43 -0600

    fix: restructure content in Slide2 component for better readability

--- app/components/slide-2.jsx
+++ app/components/slide-2.jsx
@@ -1,19 +1,21 @@
 
 export default function Slide2() {
   let contentsText=[
-    {"title":"What is the problem?"},
-    {"title":"The Vehicle Routing Problem(VRP)",
+    {"title":"What is the problem?",
       "style":{"marginBottom":"2rem","textAlign":"left"},
-      "content":"Deciding where and in what order to complete deliveries; we are working on a Green version."
-    },{
-      "style":{"textSize":"5.5rem","textAlign":"left"},
-      "content":"Companies use EV delivery vans and must plan routes that serve all customers before the batteries run out, while also choosing when and where to charge."
+      "content":[
+        "The Vehicle Routing Problem(VRP) - deciding where and in what order to complete deliveries; we are working on a Green version.",
+        "Companies use EV delivery vans and must plan routes that serve all customers before the batteries run out, while also choosing when and where to charge.",        
+      ]
     },
-    {
-      "title":"Examples",
-      "style":{"textAlign":"left"},
-      "content":"Local business, Hospitals, pharmacies, and delivery companies.",
-      "text":"This matters because we can have fewer missed deliveries, lower energy cost and emissions as well as a better performance."
+    {"title":"Examples",
+      "style":{"marginBottom":"2rem","textAlign":"left"},
+      "content":[
+        "Local business",
+        "Hospitals",
+        "Pharmacies",
+        "Delivery companies."
+      ]
     }
   ]
   
@@ -23,7 +25,7 @@ export default function Slide2() {
       {contentsText.map((item, index) => (
         <section key={index} data-auto-animate  style={item?.style}>
           <h1 className="r-fit-text">{item?.title}</h1>
-          <p >{item?.content}</p>
+          <ul>{item?.content.map((text, i) => <li key={i}>{text}</li>)}</ul>
           <p >{item?.text}</p>
         </section>
       ))}
